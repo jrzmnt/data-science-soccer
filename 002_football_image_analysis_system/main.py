771d32e42e528ad3b71a2d2ab015bde21f48f137
jrzmnt <jrzmonteiro@gmail.com>
2024-11-14 19:35:51 -0300

    feat: improved bounding box design for better player tracking and video clarity

--- 002_football_image_analysis_system/main.py
+++ 002_football_image_analysis_system/main.py
@@ -1,12 +1,24 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 
 def main():
     # Read Video
     video_frames = read_video("input_videos/08fd33_4.mp4")
 
+    # Initialize Tracker
+    tracker = Tracker("models/best.pt")
+
+    tracks = tracker.get_object_tracks(
+        video_frames, read_from_stub=True, stub_path="stubs/track_stubs.pkl"
+    )
+
+    # draw output
+    ## draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
     # Save Video
-    save_video(video_frames, "output_videos/output_video.avi")
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 
 if __name__ == "__main__":
